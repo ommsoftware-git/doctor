2de9bd262983042d6a95dee02f8004bf361ce4d0
unknown <surajjamdade83086@gmail.com>
2023-03-29 10:43:40 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,136 @@
+(function ($) {
+  "use strict";
+
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
+
+  // Initiate the wowjs
+  new WOW().init();
+
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 40) {
+      $(".navbar").addClass("sticky-top");
+    } else {
+      $(".navbar").removeClass("sticky-top");
+    }
+  });
+
+  // Dropdown on mouse hover
+  const $dropdown = $(".dropdown");
+  const $dropdownToggle = $(".dropdown-toggle");
+  const $dropdownMenu = $(".dropdown-menu");
+  const showClass = "show";
+
+  $(window).on("load resize", function () {
+    if (this.matchMedia("(min-width: 992px)").matches) {
+      $dropdown.hover(
+        function () {
+          const $this = $(this);
+          $this.addClass(showClass);
+          $this.find($dropdownToggle).attr("aria-expanded", "true");
+          $this.find($dropdownMenu).addClass(showClass);
+        },
+        function () {
+          const $this = $(this);
+          $this.removeClass(showClass);
+          $this.find($dropdownToggle).attr("aria-expanded", "false");
+          $this.find($dropdownMenu).removeClass(showClass);
+        }
+      );
+    } else {
+      $dropdown.off("mouseenter mouseleave");
+    }
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  // Date and time picker
+  $(".date").datetimepicker({
+    format: "L",
+  });
+  $(".time").datetimepicker({
+    format: "LT",
+  });
+
+  // Image comparison
+  $(".twentytwenty-container").twentytwenty({});
+
+  // Price carousel
+  $(".price-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    margin: 45,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="bi bi-arrow-left"></i>',
+      '<i class="bi bi-arrow-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 4,
+      },
+    },
+  });
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    items: 1,
+    dots: true,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="bi bi-arrow-left"></i>',
+      '<i class="bi bi-arrow-right"></i>',
+    ],
+  });
+
+  // tabs
+  $(document).ready(function () {
+    vert_tabs();
+  });
+
+  $(window).resize(function () {
+    if ($(".vertical-tabs").innerWidth() > 608) {
+      if ($("div.selected").length) {
+      } else {
+        $("div.box:first").addClass("selected");
+      }
+    }
+  });
+
+  function vert_tabs() {
+    $("ul.checklist-select li").click(function () {
+      var selectID = $(this).attr("id");
+      $("ul.checklist-select li").removeClass("active");
+      $(this).addClass("active");
+      $("div.box").removeClass("selected");
+      $("." + selectID + "-box").addClass("selected");
+    });
+  }
+})(jQuery);
